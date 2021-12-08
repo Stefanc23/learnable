fa54a183fd324315dc4aca872fd5875e07152d36
Stefanc23 <stefanocwiryana@gmail.com>
2021-12-08 16:01:09 +0700

    Add laravel sanctum authentication

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,7 +1,8 @@
 <?php
 
-use Illuminate\Http\Request;
+// use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::group(['prefix' => 'auth'], function() {
+    Route::post('login', [AuthController::class, 'login']);
+    Route::post('register', [AuthController::class, 'register']);
+    Route::group(['middleware' => 'auth:sanctum'], function() {
+        Route::post('logout', [AuthController::class, 'logout']);
+        Route::post('logoutall', [AuthController::class, 'logoutAll']);
+    });
 });
