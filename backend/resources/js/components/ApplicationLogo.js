0fd716aec66543a865578a132da3f4c5869b347b
Stefanc23 <stefanocwiryana@gmail.com>
2022-01-14 17:57:45 +0700

    Migrate web version to another repo

--- backend/resources/js/components/ApplicationLogo.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { logoSVG } from "@/images";
-
-export default function ApplicationLogo({ className }) {
-    return <img className={className} src={logoSVG} />;
-}
--- backend/resources/js/components/ApplicationLogoAlt.js
+++
@@ -1,6 +0,0 @@
-import React from "react";
-import { logoAltSVG } from "@/images";
-
-export default function ApplicationLogoAlt({ className }) {
-    return <img className={className} src={logoAltSVG} />
-}
